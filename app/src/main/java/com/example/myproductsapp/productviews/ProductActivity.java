0d5b87c53189825cbdaea54aa8db3cd893967f5e
Ramy Ashraf <Ramy3r@hotmail.com>
2023-02-04 23:58:17 +0200

    Fixed a bug where fragments appeared on top of each other :D

--- app/src/main/java/com/example/myproductsapp/productviews/ProductActivity.java
+++ app/src/main/java/com/example/myproductsapp/productviews/ProductActivity.java
@@ -2,6 +2,7 @@ package com.example.myproductsapp.productviews;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +14,9 @@ import com.example.myproductsapp.R;
 public class ProductActivity extends AppCompatActivity {
     private int productID;
     private int dest;
+    ProductListFragment productListFragment;
+    FavoritesFragment favoritesFragment;
+    ProductFragment productFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +29,30 @@ public class ProductActivity extends AppCompatActivity {
         }
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        switch (dest) {
-            case 1:
-                ProductListFragment productListFragment = new ProductListFragment();
-                fragmentTransaction.add(R.id.fragmentContainerView, productListFragment, "list");
-                break;
-            case 2:
-                FavoritesFragment favoritesFragment = new FavoritesFragment();
-                fragmentTransaction.add(R.id.fragmentContainerView, favoritesFragment, "favorites");
-                break;
-            case 3:
-                ProductFragment productFragment = new ProductFragment();
-                fragmentTransaction.add(R.id.fragmentContainerView, productFragment, "product");
-                break;
-            default:
-                finish();
-                break;
+
+        if (savedInstanceState == null) {
+            switch (dest) {
+                case 1:
+                    Toast.makeText(this, "list created", Toast.LENGTH_SHORT).show();
+                    productListFragment = new ProductListFragment();
+                    fragmentTransaction.add(R.id.fragmentContainerView, productListFragment, "list");
+                    break;
+                case 2:
+                    Toast.makeText(this, "favs created", Toast.LENGTH_SHORT).show();
+                    favoritesFragment = new FavoritesFragment();
+                    fragmentTransaction.add(R.id.fragmentContainerView, favoritesFragment, "favorites");
+                    break;
+                case 3:
+                    Toast.makeText(this, "prods created", Toast.LENGTH_SHORT).show();
+                    productFragment = new ProductFragment();
+                    fragmentTransaction.add(R.id.fragmentContainerView, productFragment, "product");
+                    break;
+                default:
+                    finish();
+                    break;
+            }
         }
+
 
         fragmentTransaction.commit();
     }
@@ -54,6 +65,5 @@ public class ProductActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-
     }
 }
